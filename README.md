c816d317cc3a484a6523a8035e70290f00accbad
Rico Fritzsche <rico.fritzsche@letsbundle.de>
2022-01-14 19:52:44 +0100

    new feature delete table by name added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # aws-dynamodb-essentials
+
+## What is this module for?
+The claim is to provide an easy-to-use abstraction layer for using AWS DynamoDB. Provides basic functionality to effectively leverage AWS DynamoDB and avoid code redundancy.
+
+## Documentation
+
+### Create a table
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ module.exports.createTable = async (params, options) => {
 
     const dynamodb = new AWS.DynamoDB();
 
-    return await new Promise((resolve, reject) =>{
+    return await new Promise((resolve, reject) => {
         dynamodb.createTable(params, (err, data) => {
             if (err){
                 if (err.message === `Table already exists: ${params.TableName}` ||
@@ -44,3 +44,26 @@ module.exports.createTable = async (params, options) => {
         })
     })
 }
+
+// Deletes the given table by its name.
+module.exports.deleteTable =  async (tableName) => {
+    if (options){
+      configureDatabase(options);
+    }
+
+    const dynamodb = new AWS.DynamoDB();
+    const params = { TableName : tableName };
+
+    return await new Promise((resolve, reject) => {
+      dynamodb.deleteTable(params, (err, data) => {
+        if (err) {
+            console.error("Unable to delete table. Error JSON:", JSON.stringify(err, null, 2));
+            reject(`ERROR :: ${err.message}`);
+        } else {
+            console.log("deleteTable. Table description JSON:", JSON.stringify(data, null, 2));
+            resolve(`SUCCESS :: Table ${params.TableName} deleted.`)
+        }
+      })
+    }   
+
+});
