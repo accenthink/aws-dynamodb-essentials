4ed3761b3479117fbb31426b0a3b5790b31de490
Rico Fritzsche <rico.fritzsche@letsbundle.de>
2022-01-14 20:05:59 +0100

    syntax error fixed

--- index.js
+++ index.js
@@ -55,14 +55,14 @@ module.exports.deleteTable =  async (tableName, options) => {
     const params = { TableName : tableName };
 
     return await new Promise((resolve, reject) => {
-      dynamodb.deleteTable(params, (err, data) => {
-        if (err) {
-            console.error("Unable to delete table. Error JSON:", JSON.stringify(err, null, 2));
-            reject(`ERROR :: ${err.message}`);
-        } else {
-            console.log("deleteTable. Table description JSON:", JSON.stringify(data, null, 2));
-            resolve(`SUCCESS :: Table ${params.TableName} deleted.`)
-        }
+        dynamodb.deleteTable(params, (err, data) => {
+          if (err) {
+              console.error("Unable to delete table. Error JSON:", JSON.stringify(err, null, 2));
+              reject(`ERROR :: ${err.message}`);
+          } else {
+              console.log("deleteTable. Table description JSON:", JSON.stringify(data, null, 2));
+              resolve(`SUCCESS :: Table ${params.TableName} deleted.`)
+          }
+        });
       });
-    }
 }
